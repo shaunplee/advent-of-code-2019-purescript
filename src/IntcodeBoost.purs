038d124cfc4544ada01f37654463ea326535982c
Shaun Lee <shaun.lee@gmail.com>
2019-12-10 06:04:03 -0800

    Day Nine complete.

--- src/IntcodeBoost.purs
+++ src/IntcodeBoost.purs
@@ -22,11 +22,14 @@ type MemState
 type IP
   = Int
 
+type RB
+  = Int
+
 type Input
   = Array BigInt
 
 type Output
-  = Maybe BigInt
+  = Array BigInt
 
 data Status
   = Running
@@ -39,10 +42,10 @@ instance statusShow :: Show Status where
   show Paused = "Paused"
 
 data ProgState
-  = ProgState MemState IP Status Input Output
+  = ProgState MemState IP RB Status Input Output
 
 instance progstateshow :: Show ProgState where
-  show (ProgState prog pos status input output) =
+  show (ProgState prog pos rb status input output) =
     "ProgState "
       <> show prog
       <> " "
@@ -87,9 +90,11 @@ parseOpMode i =
 
     { before: ops, after: params } = Str.splitAt 2 as
 
-    op = fromMaybe 0 $ I.fromString $ reverseString ops
+    op = case I.fromString $ reverseString ops of
+      Just o -> o
+      Nothing -> spy ("error parsing op " <> ops) 0
   in
-    spy "op:" $ OpMode (Opcode op) (toCharArray params)
+    OpMode (Opcode op) (toCharArray params)
 
 opInsLength :: Partial => Opcode -> Int
 opInsLength (Opcode 1) = 4 -- add
@@ -100,54 +105,61 @@ opInsLength (Opcode 5) = 3 -- JT/JNZ
 opInsLength (Opcode 6) = 3 -- JF/JZ
 opInsLength (Opcode 7) = 4 -- LT
 opInsLength (Opcode 8) = 4 -- EQ
-opInsLength (Opcode 99) = 1 -- Halt
+opInsLength (Opcode 9) = 2 -- set relative base
+opInsLength (Opcode 99) = 1 -- halt
 opInsLength (Opcode x) = spy "invalid opcode:" x
 
 
 toInt :: BigInt -> Int
-toInt bi = fromMaybe 0 $ I.fromString $ toString bi
+toInt bi = case I.fromString $ toString bi of
+  Just i -> i
+  Nothing -> spy ("error converting bigint " <> toString bi <> " to int") $ 9999
 
-getVals :: MemState -> Array BigInt -> Array Char -> Array (Maybe BigInt)
-getVals mem ps pms = map (\(Tuple p i) -> case index pms i of
-                               Just '1' -> Just p
-                               _ -> index mem (toInt p))
-                     (zip ps (0..(length ps)))
+getVals :: MemState -> RB -> Array BigInt -> Array Char -> Maybe (Array BigInt)
+getVals mem rb ps pms = traverse (\(Tuple p i) -> case index pms i of
+                                     Just '1' -> Just p
+                                     Just '2' -> index mem (rb + (toInt p))
+                                     _ -> index mem (toInt p))
+                        (zip ps (0..(length ps)))
 
 
 step :: Partial => ProgState -> ProgState
-step ps@(ProgState _ _ Halted _ _) = ps
-step (ProgState prog pos _ input output) =
+step ps@(ProgState _ _ _ Halted _ _) = ps
+step (ProgState mem pos rb _ input output) =
   let
-    (OpMode op modes) = parseOpMode (fromMaybe (fromInt 0) (index prog pos))
+    (OpMode op modes) = parseOpMode (fromMaybe (fromInt 0) (index mem pos))
 
     insLength = opInsLength op
 
-    instruction = slice pos (pos + insLength) prog
+    instruction = slice pos (pos + insLength) mem
   in case insLength of
     4 ->
       let
         [ _, a, b, dst ] = instruction
 
-        [Just aval, Just bval] = getVals prog [a, b] modes
+        Just [aval, bval] = getVals mem rb [a, b] modes
 
         val = case op of
           Opcode 1 -> aval + bval
           Opcode 2 -> aval * bval
           Opcode 7 -> if aval < bval then (fromInt 1) else (fromInt 0)
           Opcode 8 -> if aval == bval then (fromInt 1) else (fromInt 0)
-        Just newstate = modifyAt (toInt dst) (const val) prog
+        dst' = case index modes 2 of
+          Just '2' -> rb + (toInt dst)
+          _ -> toInt dst
+        Just newstate = modifyAt dst' (const val) mem
       in
-       ProgState newstate (pos + insLength) Running input output
+       ProgState newstate (pos + insLength) rb Running input output
     3 ->
       let
         [ _, a, goto] = instruction
-        [Just aval, Just gotoval] = getVals prog [a, goto] modes
+        Just [aval, gotoval] = getVals mem rb [a, goto] modes
         jump = case op of
           Opcode 5 -> aval /= (fromInt 0)
           Opcode 6 -> aval == (fromInt 0)
         newpc = if jump then (toInt gotoval) else pos + insLength
       in
-       ProgState prog newpc Running input output
+       ProgState mem newpc rb Running input output
     2 ->
       let
         [ _, dst ] = instruction
@@ -156,49 +168,58 @@ step (ProgState prog pos _ input output) =
          Opcode 3 ->
            let
              Just inval = head input
-             Just newstate = modifyAt (toInt dst) (const inval) prog
+             dst' = case head modes of
+               Just '2' -> rb + toInt dst
+               _ -> toInt dst
+             Just newstate = modifyAt dst' (const inval) mem
            in
             (ProgState
              newstate
              (pos + insLength)
+             rb
              Running
              (fromMaybe [] $ tail input)
              output)
          Opcode 4 ->
+           let Just [val] = getVals mem rb [dst] modes
+           in ProgState mem (pos + insLength) rb Paused input (snoc output val)
+         Opcode 9 ->
            let
-             [val] = getVals prog [dst] modes
+             Just [dst'] = getVals mem rb [dst] modes
            in
-            ProgState prog (pos + insLength) Paused input val
+            ProgState mem (pos + insLength) (rb + toInt dst')
+            Running input output
     1 -> case op of
-      Opcode 99 -> ProgState prog (pos + insLength) Halted input output
-    _ -> ProgState prog (pos + insLength) Halted input output
+      Opcode 99 -> ProgState mem (pos + insLength) rb Halted input output
+    _ -> ProgState mem (pos + insLength) rb Halted input output
 
 runProg :: Input -> MemState -> ProgState
 runProg input p = runProg' (newProgState input p)
   where
-  runProg' ps@(ProgState _ _ Halted _ _) = ps
-  runProg' ps@(ProgState _ _ Paused _ _) = ps
-  runProg' ps = runProg' $ spy "ps:" $ unsafePartial step ps
+  runProg' ps@(ProgState _ _ _ Halted _ _) = ps
+  runProg' ps = runProg' $ unsafePartial step ps
 
 newProgState :: Input -> MemState -> ProgState
-newProgState input p = (ProgState p 0 Running input Nothing)
+newProgState input p =
+  (ProgState (p <> replicate (65536 - (length p)) (fromInt 0))
+   0 0 Running input [])
 
 runProgState :: ProgState -> ProgState
-runProgState ps@(ProgState _ _ Halted _ _) = ps
-runProgState ps@(ProgState _ _ _ _ _) =
+runProgState ps@(ProgState _ _ _ Halted _ _) = ps
+runProgState ps@(ProgState _ _ _ _ _ _) =
   runProgState' $ unsafePartial step ps
   where
-    runProgState' ps'@(ProgState _ _ Running _ _) =
+    runProgState' ps'@(ProgState _ _ _ Running _ _) =
       runProgState' $ unsafePartial step ps'
     runProgState' ps' = ps'
 
 appendInput :: BigInt -> ProgState -> ProgState
-appendInput i (ProgState ms ip status input output) =
-  ProgState ms ip status (snoc input i) output
+appendInput i (ProgState ms ip rb status input output) =
+  ProgState ms ip rb status (snoc input i) output
 
-runOutput :: Input -> MemState -> BigInt
+runOutput :: Input -> MemState -> Array BigInt
 runOutput input p = unsafePartial case runProg input p of
-  ProgState _ _ _ _ (Just x) -> x
+  ProgState _ _ _ _ _ x -> x
 
 parseInput :: String -> Maybe MemState
 parseInput s = traverse fromString (Str.split (Pattern ",") s)
